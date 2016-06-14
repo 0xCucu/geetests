3ae84827315f8fb742cc40a9053a7e034c295dcc
zhenyangjiang <1061120731@qq.com>
2016-06-14 15:58:40 +0800

    Update geetest.php

--- src/Config/geetest.php
+++ src/Config/geetest.php
@@ -4,9 +4,9 @@ return [
     "KEY"               => "36fc3fe98530eea08dfc6ce76e3d24c4",
     "Fail_callback"     => 'Fail',// call Results/Fail
     "Success_callback"  => 'Success',// call Results/Success
-    "user_id"           =>'',
-    "api_url"           =>"http://api.geetest.com/register.php?gt={ID}&user_id={user_id}",
-    "connectTimeout"    =>'1',
-    "socketTimeout"   =>'1'
+    "user_id"           => '',
+    "api_url"           => "http://api.geetest.com/register.php?gt={ID}&user_id={user_id}",
+    "connectTimeout"    => '1',
+    "socketTimeout"     => '1'
 ];
 ?>
